10b427d3e5949b48b9719803828774bcafe4d8ef
Nine-H <nine.gentooman@gmail.com>
2019-09-24 12:15:04 +1000

    add event management to the student panel

--- jsonapi/admin.py
+++ jsonapi/admin.py
@@ -5,13 +5,19 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import *
 
-class StudentEventAdmin(admin.ModelAdmin):
-     list_filter = ('event', 'student')
+# class StudentEventAdmin(admin.ModelAdmin):
+#      list_filter = ('event', 'student')
+
+class StudentEventInline(admin.TabularInline):
+    model = StudentEvent
+
+class StudentAdmin(admin.ModelAdmin):
+    inlines = (StudentEventInline,)
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Course)
 admin.site.register(Event)#, EventAdmin)
-admin.site.register(StudentEvent, StudentEventAdmin)
-admin.site.register(Student)
+#admin.site.register(StudentEvent, StudentEventAdmin)
+admin.site.register(Student, StudentAdmin)
 admin.site.register(Clinician)
 admin.site.register(Tile)
