8f278c9076789e2671c5066b63419b37e8c7dfc7
Nine-H <nine.gentooman@gmail.com>
2019-10-14 16:10:31 +1100

    create a student when a user signs up

--- jsonapi/forms.py
+++ jsonapi/forms.py
@@ -7,3 +7,14 @@ class StudentSignUpForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         model = User
+    
+    @transaction.atomic
+    def save(self):
+        user = super().save()
+        student = Student.objects.create(
+            student_user=user,
+            student_name="Test",
+            student_id="30333333",
+            student_phone="0400000000",
+            student_email="fake123@fake.com")
+        return user
