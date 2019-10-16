067ea7ee1290231f59c929c586cabfa8f348d4a7
Nine-H <nine.gentooman@gmail.com>
2019-10-16 19:24:47 +1100

    added repeating events

--- jsonapi/models.py
+++ jsonapi/models.py
@@ -50,6 +50,7 @@ class CourseSerializer(serializers.ModelSerializer):
         fields = ['course_code', 'course_name']
 
 class Event(models.Model):
+    event_parent = models.ForeignKey('self', blank=True, null=True, default=None, on_delete=models.PROTECT)
     event_title = models.CharField(max_length=200)
     event_course = models.ForeignKey(Course, on_delete=models.PROTECT)
     event_clinician = models.ForeignKey(Clinician, on_delete=models.PROTECT)
@@ -58,6 +59,25 @@ class Event(models.Model):
     event_finishtime = models.DateTimeField('Ending time')
     event_repeating = models.BooleanField(default=True)
     event_frequency = models.PositiveSmallIntegerField()
+    
+    def save(self, *args, **kwargs):
+        super(Event, self).save(*args, **kwargs)
+        if self.event_repeating:
+            Event.objects.filter(event_parent=self).delete()
+            temp_starttime = self.event_starttime
+            while temp_starttime.date() < Config.objects.get(id=1).ending_date:
+                temp_starttime += timedelta(days=self.event_frequency)
+                child_event = Event.objects.create(
+                    event_parent = self,
+                    event_title = self.event_title,
+                    event_course = self.event_course,
+                    event_clinician = self.event_clinician,
+                    event_location = self.event_location,
+                    event_starttime = temp_starttime,
+                    event_finishtime = temp_starttime + timedelta(hours=2),
+                    event_repeating = False,
+                    event_frequency = self.event_frequency)
+                child_event.save()
 
     def __str__(self):
         return " - ".join([self.event_title, self.event_clinician.clinician_name])
@@ -138,38 +158,16 @@ class StudentEventViewSet(viewsets.ModelViewSet):
 class Config(models.Model):
     starting_date = models.DateField(default=date.today())
     ending_date = models.DateField(default=date.today()+timedelta(weeks=12))
-    
+
     # By overriding these methods config becomes a singleton.
     def save(self, *args, **kwargs):
         self.pk = 1
         super(Config, self).save(*args, **kwargs)
-    
+
     def delete(self, *args, **kwargs):
         pass
-    
+
     @classmethod
     def load(cls):
         obj, created = cls.objects.get_or_create(pk=1)
         return obj
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
