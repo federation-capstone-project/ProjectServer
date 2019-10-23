86c66441abda7fdf306ee8097f87d8d1aabb69aa
Nathan Blaney <nathan@blaney.xyz>
2019-10-24 09:11:21 +1100

    Update urls.py

--- jsonapi/urls.py
+++ jsonapi/urls.py
@@ -5,6 +5,7 @@ from .models import *
 from .views import *
 from rest_framework import routers, serializers, viewsets
 from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework.authtoken import views
 
 router = routers.DefaultRouter()
 router.register(r'events', EventViewSet, basename='Events')
@@ -12,7 +13,7 @@ router.register(r'studentevent', StudentEventViewSet, basename='StudentEvents')
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
-    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-auth/', views.obtain_auth_token),
     url(r'^', include(router.urls)),
     url(r'^signup', StudentSignUpView.as_view(), name='registration'),
     url(r'^mycourses/(?P<student>.+)/$', MyCourseList.as_view()),
